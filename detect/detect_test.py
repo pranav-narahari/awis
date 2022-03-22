7d8a8634887153ae1389a372acca0bef161777d6
Narahari <narprana@3c22fb089330.ant.amazon.com>
2022-03-22 16:10:28 -0700

    a

--- detect/detect_test.py
+++ detect/detect_test.py
@@ -38,8 +38,6 @@ if __name__ == "__main__":
    
     args = parser.parse_args()
 
-    label_file = os.path.join(default_model_dir, 'coco_labels.txt')
-
     conf_thresh = 0.25
     iou_thresh = 0.45
 
@@ -54,15 +52,18 @@ if __name__ == "__main__":
     input_details = interpreter.get_input_details()
     output_details = interpreter.get_output_details()
 
+    print(input_details[0]['quantization'][1])
+    print(input_details[0]['quantization'][0])
+    print(output_details[0]['quantization'][1])
+    print(output_details[0]['quantization'][0])
+
     with open(args.labels, 'r') as f:
         data = yaml.load(f, Loader=yaml.SafeLoader)
 
     labels = data['names']
-    print(labels)
     logger.info("Loaded {} classes".format(len(labels)))
 
     size = common.input_size(interpreter)
-    print(size[0])
     
     cam = cv2.VideoCapture(args.device)
     
@@ -90,12 +91,6 @@ if __name__ == "__main__":
             interpreter.invoke()
             interpreter_output = interpreter.get_tensor(output_details[0]["index"])
             classes = classify.get_classes(interpreter, top_k=1)
-
-            labels = dataset.read_label_file(label_file)
-            for c in classes:
-                print('%s: %.5f' % (labels.get(c.id, c.id), c.score))
-
-
 
         except KeyboardInterrupt:
             break
