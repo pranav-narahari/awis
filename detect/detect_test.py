b479b774f3e9327aea90ff2c65d5f7056a539abd
Narahari <narprana@3c22fb089330.ant.amazon.com>
2022-03-22 16:46:16 -0700

    a

--- detect/detect_test.py
+++ detect/detect_test.py
@@ -91,7 +91,7 @@ if __name__ == "__main__":
             interpreter_output = interpreter.get_tensor(output_details[0]["index"])
             result = output_scale * (interpreter_output.astype('float32') - output_zero_point)
 
-            print(pred[0] - result[0])
+            print(np.prod(pred[0] - result[0]))
             break
 
             if len(result):
