951ee26931ce759cb716b3ff5e1c3611cfc18955
Narahari <narprana@3c22fb089330.ant.amazon.com>
2022-03-23 02:22:01 -0700

    a

--- detect/detect_test.py
+++ detect/detect_test.py
@@ -110,11 +110,11 @@ if __name__ == "__main__":
             interpreter.invoke()
             interpreter_output = interpreter.get_tensor(output_details[0]["index"])
             result = output_scale * (interpreter_output.astype('float32') - output_zero_point)
-            nms_result = non_max_suppression(result, conf_thresh, iou_thresh)
+            nms_result = non_max_suppression(result)
 
             full_image, net_image, pad = utils.get_image_tensor(image, input_size_old[0])
 
-            pred = model_old.forward(net_image)
+            # pred = model_old.forward(net_image)
 
             # nms_result = pred
             print(nms_result)
