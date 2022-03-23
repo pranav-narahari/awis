bbee732c9a02fee94aa2cf1bce36086cd64082c1
Narahari <narprana@3c22fb089330.ant.amazon.com>
2022-03-23 02:54:54 -0700

    a

--- detect/nms.py
+++ detect/nms.py
@@ -69,7 +69,7 @@ def non_max_suppression(prediction, conf_thres=0.25, iou_thres=0.45, classes=Non
 
     t = time.time()
     output = [np.zeros((0, 6))] * prediction.shape[0]
-    print(output.shape)
+    print(output)
     for xi, x in enumerate(prediction):  # image index, image inference
         # Apply constraints
         # x[((x[..., 2:4] < min_wh) | (x[..., 2:4] > max_wh)).any(1), 4] = 0  # width-height
