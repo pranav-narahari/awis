0328ca915a83756426d7c7bb46919c5acb784c69
Narahari <narprana@3c22fb089330.ant.amazon.com>
2022-03-23 02:16:44 -0700

    a

--- detect/detect_test.py
+++ detect/detect_test.py
@@ -118,7 +118,6 @@ if __name__ == "__main__":
 
             # nms_result = pred
             print(nms_result)
-            break
 
             if len(nms_result[0]):
                 # Rescale boxes from img_size to im0 size
