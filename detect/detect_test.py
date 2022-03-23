375ff3cf7eff70c2110110cc17788416bcabefec
Narahari <narprana@3c22fb089330.ant.amazon.com>
2022-03-23 01:25:53 -0700

    a

--- detect/detect_test.py
+++ detect/detect_test.py
@@ -10,6 +10,7 @@ from pycoral.utils import edgetpu
 from pycoral.adapters import common
 from pycoral.adapters import classify
 from pycoral.utils import dataset
+from pycoral.adapters.detect import get_objects
 
 import numpy as np
 from tqdm import tqdm
@@ -18,6 +19,22 @@ import yaml
 
 import utils
 from edgetpumodel import EdgeTPUModel
+
+def append_objs_to_img(cv2_im, inference_size, objs, labels):
+    height, width, channels = cv2_im.shape
+    scale_x, scale_y = width / inference_size[0], height / inference_size[1]
+    for obj in objs:
+        bbox = obj.bbox.scale(scale_x, scale_y)
+        x0, y0 = int(bbox.xmin), int(bbox.ymin)
+        x1, y1 = int(bbox.xmax), int(bbox.ymax)
+
+        percent = int(100 * obj.score)
+        label = '{}% {}'.format(percent, labels.get(obj.id, obj.id))
+
+        cv2_im = cv2.rectangle(cv2_im, (x0, y0), (x1, y1), (0, 255, 0), 2)
+        cv2_im = cv2.putText(cv2_im, label, (x0, y0+30),
+                             cv2.FONT_HERSHEY_SIMPLEX, 1.0, (255, 0, 0), 2)
+    return cv2_im
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
@@ -116,11 +133,14 @@ if __name__ == "__main__":
                 
                 logger.info("Detected: {}".format(s))
 
-                for *xyxy, conf, cls in reversed(result[0]):
-                    c = int(cls)  # integer class
-                    label = f'{labels[c]} {conf:.2f}'
-                    output_image = utils.plot_one_box(xyxy, image, label=label)
-                cv2.imshow('frame', output_image)
+                # for *xyxy, conf, cls in reversed(result[0]):
+                #     c = int(cls)  # integer class
+                #     label = f'{labels[c]} {conf:.2f}'
+                #     output_image = utils.plot_one_box(xyxy, image, label=label)
+
+                objs = get_objects(interpreter, conf_thresh)[:3]
+                cv2_im = append_objs_to_img(cv2_im, size, objs, labels)
+                cv2.imshow('frame', cv2_im)
                 if cv2.waitKey(1) & 0xFF == ord('q'):
                     break
 
