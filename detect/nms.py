f379d5024381aa48f60eceef1c366ede9a9b46f5
Narahari <narprana@3c22fb089330.ant.amazon.com>
2022-03-23 03:15:55 -0700

    a

--- detect/nms.py
+++ detect/nms.py
@@ -55,6 +55,8 @@ def non_max_suppression(prediction, conf_thres=0.25, iou_thres=0.45, classes=Non
     nc = prediction.shape[2] - 5  # number of classes
     xc = prediction[..., 4] > conf_thres  # candidates
 
+    print(conf_thres)
+
     # Checks
     assert 0 <= conf_thres <= 1, f'Invalid Confidence threshold {conf_thres}, valid values are between 0.0 and 1.0'
     assert 0 <= iou_thres <= 1, f'Invalid IoU {iou_thres}, valid values are between 0.0 and 1.0'
