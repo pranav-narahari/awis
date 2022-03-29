f337921cd5a6bdd22738c7055830320ffc79ceef
Narahari <narprana@3c22fb089330.ant.amazon.com>
2022-03-29 15:26:14 -0700

    added usb functionality

--- capture/capture.py
+++ capture/capture.py
@@ -19,7 +19,7 @@ def create_dir(usb):
         output_dir = "../data"
     else:
         #Checking USB disk 
-        if not os.path.exists(os.path.join("/media/usb/data")):
+        if not os.path.exists(os.path.join("/media/usb")):
             logger.error("USB Not Connected")
             exit()
         
